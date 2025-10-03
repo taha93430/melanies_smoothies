202546efd5db08b38ffc9633be5f91636b8036e5
taha93430 <taha93430@gmail.com>
2025-10-03 20:12:03 +0200

    Update streamlit_app.py
    move to ingredients_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,6 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 
-# New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
 #   st.write(ingrediens_list)
 #   st.text(ingrediens_list)
@@ -38,6 +33,8 @@ if ingredients_list:
 
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
    #st.write(ingredients_string)
     
